625f28b2638dc62b4f8d1fe1e9b1a15886957c30
Luz917 <53020308+Luz917@users.noreply.github.com>
2020-11-23 01:55:15 -0500

    Add files via upload

---
+++ module6/d3_lab/01answer.js
@@ -0,0 +1,32 @@
+
+d3.csv('https://raw.githubusercontent.com/Luz917/data608/master/ue_industry.csv', data => {
+
+    // Define your scales and generator here.
+
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([20, 1180]);
+
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
+
+    let line_ans = d3.line()
+        .x(d => xScale(d.index))
+        .y(d => yScale(d.Agriculture))
+
+    d3.select('#answer1')
+        // append more elements here
+        .append('path')
+        .attr('stroke', 'black')
+        .attr('stroke-width', '1px')
+        .attr('d', line_ans(data))
+
+
+});
+
+
+
+
+
+
